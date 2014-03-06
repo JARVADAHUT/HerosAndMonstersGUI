928e4ea232dbee79ea306f033c41ba06c03c6d77
Andrew McCall <mccall.andrew@gmail.com>
2014-03-05 18:51:42 -0800

    Check out the PublicEnums.cs EnumAttacks class. I'm not 100% it will work, but if it does, we've got a sick way of making enums more useful.

--- HerosAndMostersGUI/BattleCode/PublicEnums.cs
+++ HerosAndMostersGUI/BattleCode/PublicEnums.cs
@@ -1,4 +1,9 @@
-﻿namespace DesignPatterns___DC_Design
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace DesignPatterns___DC_Design
 {
     public enum StatsType
     {
@@ -65,8 +70,46 @@
         Max //<--- For generating items, put new types before this
     }
 
-    public class EnumAttacks
+    public class EnumAttacks : IEnumerable<EnumAttacks>
     {
+        static EnumAttacks()
+        {
+            Type type = typeof(EnumAttacks);
+            var fields = type.GetFields(BindingFlags.Public|BindingFlags.Static);
+            AttacksList = new List<EnumAttacks>();
+            foreach (var field in fields)
+            {
+                AttacksList.Add((EnumAttacks)field.GetValue(typeof(EnumAttacks)));
+            }
+        }
+
+        private static readonly List<EnumAttacks> AttacksList; 
+
+        public static readonly EnumAttacks StrongAttack = new EnumAttacks("Strong Attack","Performs a strong attack against a selected opponent.");
+        public static readonly EnumAttacks WeakAttack = new EnumAttacks("Weak attack","Performs a weak attack against an opponent.");
+        public static readonly EnumAttacks PitifulHeal = new EnumAttacks("Pitiful heal","Performs a minute heal on selected character.");
         
+        public static readonly EnumAttacks Fireball = new EnumAttacks("Fireball","Engulfs the target monster and surrounding monsters in flames!");
+
+
+
+        private EnumAttacks(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public string Description { private set; get; }
+        public string Name { private set; get; }
+
+        public IEnumerator<EnumAttacks> GetEnumerator()
+        {
+            return AttacksList.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 }
